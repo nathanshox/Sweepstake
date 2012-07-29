d3d489f6b05a83e4c560abc29a3700e0f080f194
Nathan Shaughnessy <nathanshox@gmail.com>
2012-07-29 23:56:55 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sweekstake::Application.config.secret_token = '3a81e981e608f0f16e4cd0468265d52e1084e1e53d8f1840cb17c805fecbaf75e0df68c51a4a8cd93de039b008f940fb5c168178148f3aaa477491bf01c5a865'
