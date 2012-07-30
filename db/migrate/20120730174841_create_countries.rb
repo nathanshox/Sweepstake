8dfbc7c9c37c62610af35e930af93adb1176deab
Nathan Shaughnessy <nathanshox@gmail.com>
2012-07-30 18:58:48 +0100

    Default country medal, rank, and points values to 0

--- db/migrate/20120730174841_create_countries.rb
+++ db/migrate/20120730174841_create_countries.rb
@@ -2,13 +2,13 @@ class CreateCountries < ActiveRecord::Migration
   def change
     create_table :countries do |t|
       t.string :name
-      t.integer :rank
-      t.integer :gold
-      t.integer :silver
-      t.integer :bronze
-      t.decimal :points
-      t.decimal :factor
-      t.decimal :total_points
+      t.integer :rank, :default => 0
+      t.integer :gold, :default => 0
+      t.integer :silver, :default => 0
+      t.integer :bronze, :default => 0
+      t.decimal :points, :default => 0
+      t.decimal :factor, :default => 0
+      t.decimal :total_points, :default => 0
 
       t.timestamps
     end
