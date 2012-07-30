ef110c72a7d3645bf91d4bc400263a900303d4ee
Nathan Shaughnessy <nathanshox@gmail.com>
2012-07-30 19:10:39 +0100

    Calculate country's points on save

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,12 @@
 class Country < ActiveRecord::Base
   attr_accessible :bronze, :factor, :gold, :name, :rank, :silver
+
+  before_save :calculate_points
+
+  private
+
+    def calculate_points
+      self.points = (self.gold * 10) + (self.silver * 5) + (self.bronze * 3)
+      self.total_points = self.points * self.factor
+    end
 end
