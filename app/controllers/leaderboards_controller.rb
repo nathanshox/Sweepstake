20b91640d458f297461c9fc944ec043eb8877205
Nathan Shaughnessy <nathanshox@gmail.com>
2012-08-01 19:58:58 +0100

    Can sort countries by columns

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,11 +1,36 @@
 class LeaderboardsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def countries
-    @ranked = Country.where("rank is not null").order("rank ASC")
-    @not_ranked = Country.where("rank is null")
-    @countries = @ranked + @not_ranked
+
+    params[:sort] = 'rank' if params[:sort].nil?
+    
+    if params[:sort] && params[:sort] != 'rank'
+      @countries = Country.order(sort_column + " " + sort_direction)
+    else
+      @not_ranked = Country.where("rank is null")
+
+      if params[:direction] == 'asc' || params[:direction].nil?
+        @ranked = Country.where("rank is not null").order("rank ASC")
+        @countries = @ranked + @not_ranked
+      else
+        @ranked = Country.where("rank is not null").order("rank DESC")
+        @countries = @not_ranked + @ranked
+      end
+    end
   end
 
   def people
     @people = Person.order("points DESC")
+  end
+
+  private
+  
+  def sort_column
+    Country.column_names.include?(params[:sort]) ? params[:sort] : "rank"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
